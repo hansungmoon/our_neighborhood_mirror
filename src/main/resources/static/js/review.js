5c1f9a91aaaca644075503bff7e841de9ad2ec62
hansungmoon <arnold1998@naver.com>
2022-11-25 00:50:35 +0900

    no message

--- src/main/resources/static/js/review.js
+++ src/main/resources/static/js/review.js
@@ -78,14 +78,14 @@ var main = {
 
             if(likeBtn.hasClass("active")){
                 _this.likeUpdate(true)
-                this.find("img").attr({
+                $(this).find("img").attr({
                     "src": "https://cdn-icons-png.flaticon.com/512/803/803087.png",
                     alt:"찜하기 완료"
                 });
             }else{
                 _this.likeUpdate(false)
-                this.find("i").removeClass("fas").addClass("far")
-                this.find("img").attr({
+                $(this).find("i").removeClass("fas").addClass("far")
+                $(this).find("img").attr({
                     "src": "https://cdn-icons-png.flaticon.com/512/812/812327.png",
                     alt:"찜하기"
                 })
