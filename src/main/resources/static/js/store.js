869811b00869e8edf27e6da30fc7e62b778271d1
hansungmoon <arnold1998@naver.com>
2022-11-28 22:36:41 +0900

    no message

--- src/main/resources/static/js/store.js
+++ src/main/resources/static/js/store.js
@@ -252,6 +252,7 @@ var main = {
             }
 
             this.changeMainCategories(this.mainChildren);
+            this.changeMidCategories(this.midChildren);
 
         }).catch((e) => {
             console.error(e);
@@ -300,6 +301,7 @@ var main = {
                     }
                 }
             }
+
             this.changeMidCategories(this.midChildren);
 
         });
@@ -407,7 +409,7 @@ var main = {
         }
 
         subParentId = this.categoryLayerEl.mid.options
-                [this.categoryLayerEl.mid.selectedIndex].value;
+            [this.categoryLayerEl.mid.selectedIndex].value;
         console.log("subParentId = ", subParentId)
 
 
