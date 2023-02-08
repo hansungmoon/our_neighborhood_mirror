d25b92fdb2fd26334de613084e907f74794d9769
hansungmoon <arnold1998@naver.com>
2023-02-08 23:43:39 +0900

    no message

--- src/test/java/ywphsm/ourneighbor/unit/repository/RequestAddStoreRepositoryTest.java
+++ src/test/java/ywphsm/ourneighbor/unit/repository/RequestAddStoreRepositoryTest.java
@@ -8,11 +8,19 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import ywphsm.ourneighbor.domain.embedded.Address;
+import ywphsm.ourneighbor.domain.embedded.BusinessTime;
 import ywphsm.ourneighbor.domain.store.RequestAddStore;
 import ywphsm.ourneighbor.domain.store.Review;
+import ywphsm.ourneighbor.domain.store.Store;
 import ywphsm.ourneighbor.repository.requestaddstore.RequestAddStoreRepository;
 
+import java.time.LocalTime;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.geolatte.geom.builder.DSL.g;
+import static org.geolatte.geom.builder.DSL.point;
+import static org.geolatte.geom.crs.CoordinateReferenceSystems.WGS84;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -38,6 +46,24 @@ public class RequestAddStoreRepositoryTest {
         RequestAddStore savedRequestAddStore = requestAddStoreRepository.save(requestAddStore);
 
         assertThat(savedRequestAddStore.getContent()).isEqualTo(requestAddStore.getContent());
+    }
+
+    @Test
+    @DisplayName("가게추가 요청 삭제시 IsEmpty 확인")
+    void should_IsEmpty_When_DeleteARequestAddStore() {
+        RequestAddStore requestAddStore = RequestAddStore.builder()
+                .name("가게1")
+                .address(new Address("테스트 로", "11111", "48017", "상세 주소"))
+                .content("가게요청1")
+                .build();
+
+        tem.persist(requestAddStore);
+
+        requestAddStoreRepository.deleteById(requestAddStore.getId());
+
+        Optional<RequestAddStore> findRequestAddStore = requestAddStoreRepository.findById(requestAddStore.getId());
+
+        assertThat(findRequestAddStore).isEmpty();
     }
 
 }
