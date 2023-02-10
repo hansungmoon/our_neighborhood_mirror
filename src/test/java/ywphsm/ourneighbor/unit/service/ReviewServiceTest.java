0748eab6891f7aa578ccf18190b94d94d98d8c41
hansungmoon <arnold1998@naver.com>
2023-02-10 17:30:19 +0900

    no message

--- src/test/java/ywphsm/ourneighbor/unit/service/ReviewServiceTest.java
+++ src/test/java/ywphsm/ourneighbor/unit/service/ReviewServiceTest.java
@@ -1,4 +1,18 @@
 package ywphsm.ourneighbor.unit.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ywphsm.ourneighbor.repository.review.ReviewRepository;
+import ywphsm.ourneighbor.service.ReviewService;
+
+@ExtendWith(MockitoExtension.class)
 public class ReviewServiceTest {
+
+    @Mock
+    ReviewRepository reviewRepository;
+
+    @InjectMocks
+    ReviewService reviewService;
 }
