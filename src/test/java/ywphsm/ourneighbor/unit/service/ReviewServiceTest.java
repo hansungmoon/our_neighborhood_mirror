dd589d5754c9b7c005006867bfe2c1ed7a9b4f15
hansungmoon <arnold1998@naver.com>
2023-02-16 23:53:30 +0900

    no message

--- src/test/java/ywphsm/ourneighbor/unit/service/ReviewServiceTest.java
+++ src/test/java/ywphsm/ourneighbor/unit/service/ReviewServiceTest.java
@@ -8,8 +8,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 import ywphsm.ourneighbor.domain.dto.ReviewDTO;
 import ywphsm.ourneighbor.domain.dto.hashtag.HashtagDTO;
+import ywphsm.ourneighbor.domain.file.UploadFile;
 import ywphsm.ourneighbor.domain.hashtag.Hashtag;
 import ywphsm.ourneighbor.domain.member.Member;
 import ywphsm.ourneighbor.domain.store.Review;
@@ -19,11 +22,15 @@ import ywphsm.ourneighbor.repository.review.ReviewRepository;
 import ywphsm.ourneighbor.repository.store.StoreRepository;
 import ywphsm.ourneighbor.service.ReviewService;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -86,5 +93,62 @@ public class ReviewServiceTest {
         then(storeRepository).should().findWithOptimisticLockById(mockStoreId);
         then(memberRepository).should().findById(mockMemberId);
         then(reviewRepository).should().save(any());
+    }
+
+    @Test
+    @DisplayName("리뷰 삭제")
+    void should_DeleteReview_When_ByReviewId() throws IOException {
+        // given
+        Store store = Store.builder()
+                .name("매장")
+                .build();
+
+        Long mockStoreId = 2L;
+
+        setField(store, "id", mockStoreId);
+
+        ReviewDTO.Add dto = ReviewDTO.Add.builder()
+                .content("리뷰내용")
+                .rating(5)
+                .storeId(mockStoreId)
+                .build();
+
+        Long mockReviewId = 2L;
+
+        setField(dto, "reviewId", mockReviewId);
+
+//        List<MultipartFile> file = new ArrayList<>();
+//        file.add(new MockMultipartFile("file", "test.png", "image/png", new FileInputStream("C:/Users/HOME/Desktop/JAVA/menu_file/5cf53790-54a5-4c5f-9709-0394d58cec94.png")));
+
+        Review entity = dto.toEntity();
+
+        UploadFile uploadFile1 = new UploadFile("업로드명1", "저장명1", "URL1");
+        List<UploadFile> fileList = new ArrayList<>();
+        fileList.add(uploadFile1);
+        setField(entity, "fileList", fileList);
+
+        given(reviewRepository.findById(mockReviewId)).willReturn(Optional.of(entity));
+        given(storeRepository.findWithOptimisticLockById(mockStoreId)).willReturn(Optional.of(store));
+
+        // when
+        Long reviewId = reviewService.delete(mockStoreId, mockReviewId);
+
+        // then
+        assertThat(dto.getReviewId()).isEqualTo(reviewId);
+        then(reviewRepository).should().findById(reviewId);
+        then(storeRepository).should().findWithOptimisticLockById(mockStoreId);
+        then(reviewRepository).should().delete(entity);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 리뷰 reviewId로 조회 시 예외 발생")
+    void should_ThrowException_When_ExistsNotReviewById() {
+        // given
+        Long mockReviewId = 1L;
+        given(reviewRepository.findById(1L)).willThrow(new IllegalArgumentException());
+
+        // then
+        assertThatThrownBy(() -> reviewService.findById(mockReviewId)).isInstanceOf(IllegalArgumentException.class);
+        then(reviewRepository).should().findById(mockReviewId);
     }
 }
