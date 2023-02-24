d1c1df7def6662d016ff1fe3304173a432a376d7
hansungmoon <arnold1998@naver.com>
2023-02-24 22:26:06 +0900

    no message

--- src/test/java/ywphsm/ourneighbor/unit/service/MemberServiceTest.java
+++ src/test/java/ywphsm/ourneighbor/unit/service/MemberServiceTest.java
@@ -1,6 +1,5 @@
 package ywphsm.ourneighbor.unit.service;
 
-import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,26 +10,18 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.env.Environment;
 import org.springframework.mock.web.MockMultipartFile;
 import ywphsm.ourneighbor.domain.dto.Member.MemberDTO;
-import ywphsm.ourneighbor.domain.dto.ReviewDTO;
 import ywphsm.ourneighbor.domain.file.AwsS3FileStore;
 import ywphsm.ourneighbor.domain.file.UploadFile;
 import ywphsm.ourneighbor.domain.member.Member;
-import ywphsm.ourneighbor.domain.store.Review;
-import ywphsm.ourneighbor.domain.store.Store;
 import ywphsm.ourneighbor.repository.member.MemberRepository;
-import ywphsm.ourneighbor.service.ReviewService;
 import ywphsm.ourneighbor.service.member.MemberService;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.mock;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 @ExtendWith(MockitoExtension.class)
@@ -79,6 +70,25 @@ public class MemberServiceTest {
 
         // then
         then(awsS3FileStore).should().storeFile(dto.getFile());
+        then(memberRepository).should().save(any());
+    }
+
+    @Test
+    @DisplayName("API 로그인 회원 저장")
+    void should_SaveAPIMember() throws IOException {
+        // given
+        UploadFile uploadFile = new UploadFile("업로드명1", "저장명1", "URL1");
+
+        MemberDTO.ApiAdd dto = MemberDTO.ApiAdd.builder()
+                .birthDate("19991002")
+                .build();
+
+        given(memberRepository.save(any())).willReturn(dto.toEntity(25));
+
+        // when
+        memberService.apiSave(dto, uploadFile);
+
+        // then
         then(memberRepository).should().save(any());
     }
 
